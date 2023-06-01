853b90d2debd955278b61186e1ed40bc836def45
Olga Shimanskaia <oshimanskaia@google.com>
2023-06-01 18:51:17 +0200

    try supabase login

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -10,8 +10,8 @@ const Profile = () => {
 
   const handleClick = async () => {
     await supabase.from('helpers').delete().eq('id', user?.id).select();
-    await supabase.from('profile').update({role: 'user'}).eq('id', user?.id);
-    
+    await supabase.from('profiles').update({role: 'user'}).eq('id', user?.id);
+
     push({
       pathname: '/',
     });
